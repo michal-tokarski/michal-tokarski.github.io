9693c15e406b79cd9c110bdd7917b7e39362080e
michal-tokarski <michal_tokarski@wp.pl>
2020-09-24 22:30:11 +0200

    update to push to trello

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
   // const apiRoot = 'http://localhost:8080/v1/task/';
   const apiRoot = 'https://mysterious-headland-08444.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://mysterious-headland-08444.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
